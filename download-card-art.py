37e5f2fad677115593acb350c7769f2f7b9d7468
ranni <101507816+ranni0225@users.noreply.github.com>
2026-04-16 13:03:11 +0800

    feat: replace "requests" library with "httpx"

--- download-card-art.py
+++ download-card-art.py
@@ -1,17 +1,14 @@
 # encoding: UTF-8
 
 
+import asyncio
 import datetime
+import httpx
 import json
 import logging
 import sys
 
-import requests
-
-from concurrent.futures import as_completed, ThreadPoolExecutor
 from pathlib import Path
-
-from requests.adapters import HTTPAdapter
 
 # ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== =====
 # HELPER FUNCTIONS
@@ -71,10 +68,6 @@ logging.addLevelName(logging.ERROR, "e")
 logging.addLevelName(logging.CRITICAL, "c")
 logging.basicConfig(level=logging.INFO, format="[%(asctime)s] [%(levelname)s] %(message)s", datefmt="%H:%M:%S", stream=sys.stdout)
 
-session = requests.Session()
-session.mount("http://", HTTPAdapter(pool_connections=WORKER_COUNT, pool_maxsize=WORKER_COUNT, max_retries=RETRY_COUNT))
-session.mount("https://", HTTPAdapter(pool_connections=WORKER_COUNT, pool_maxsize=WORKER_COUNT, max_retries=RETRY_COUNT))
-
 manifestFile = Path("manifest.json")
 manifest = LoadJson(manifestFile)
 
@@ -91,109 +84,118 @@ CARD_ART_STAGE2_URL_SUFFIX = "/card_after_training.png"
 
 JST_TIMEZONE = datetime.timezone(datetime.timedelta(hours=9))
 
+
 # ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== =====
 # DOWNLOADER LOGIC
 # ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== =====
 
-logging.info("Creating required directories...")
+async def main():
+    logging.info("Creating required directories...")
 
-requiredDirectories = [
-    CARD_ART_DIRECTORY
-]
-for requiredDirectory in requiredDirectories:
-    requiredDirectory.mkdir(parents=True, exist_ok=True)
-
-logging.info("Fetching card data...")
-
-try:
-    response = session.get(CARD_DATA_URL, timeout=DEFAULT_TIMEOUT)
-    response.raise_for_status()
-    cards = response.json()
-except Exception as e:
-    logging.error(f"Failed to fetch card data: {e}", exc_info=True)
-    sys.exit(-1)
-
-logging.info("Fetching game character data...")
-
-try:
-    response = session.get(GAME_CHARACTER_DATA_URL, timeout=DEFAULT_TIMEOUT)
-    response.raise_for_status()
-    gameCharacters = response.json()
-except Exception as e:
-    logging.error(f"Failed to fetch game character data: {e}", exc_info=True)
-    sys.exit(-1)
-
-characterNames = {
-    gameCharacter["id"]: f"{gameCharacter.get('firstName', '')} {gameCharacter.get('givenName', '')}".strip() for gameCharacter in gameCharacters
-}
-
-logging.info("Downloading card art...")
-
-
-def DownloadCardArt(url: str, filePath: Path) -> bool:
-    filePath.parent.mkdir(parents=True, exist_ok=True)
-
-    if manifest.get(filePath.name):
-        return True
-
-    try:
-        response = session.get(url, stream=True, timeout=DEFAULT_TIMEOUT)
-
-        if response.status_code == 404:
-            return True
-        response.raise_for_status()
-
-        logging.info(f"Downloading \"{filePath.name}\"")
-
-        with filePath.open("wb") as file:
-            for chunk in response.iter_content(chunk_size=4096):
-                file.write(chunk)
-
-        manifest[filePath.name] = url
-
-        return True
-    except Exception as e:
-        logging.error(f"Failed to download card art \"{url}\" to \"{filePath}\": {e}", exc_info=True)
-
-        if filePath.exists():
-            filePath.unlink()
-
-        return False
-
-
-def ProcessSingleCard(card: dict):
-    cardId = card.get("id", "")
-    cardAssetId = card.get("assetbundleName", "")
-
-    cardReleaseTime = datetime.datetime.fromtimestamp(card.get("releaseAt", 0) / 1000, tz=JST_TIMEZONE).strftime("%Y%m%d")
-    cardRarity = card.get("cardRarityType", "rarity_none").split('_')[-1].lower()
-    cardName = card.get("prefix", "")
-    cardCharacterId = card.get("characterId", "")
-
-    cardCharacterName = characterNames.get(cardCharacterId, "")
-
-    cardIsTrainable = cardRarity in [
-        "3",
-        "4",
+    requiredDirectories = [
+        CARD_ART_DIRECTORY
     ]
-    cardReleaseYear = cardReleaseTime[:4]
+    for requiredDirectory in requiredDirectories:
+        requiredDirectory.mkdir(parents=True, exist_ok=True)
 
-    logging.info(f"Processing card #{cardId}({cardAssetId}):\t [{cardReleaseTime}] {cardCharacterName} - {cardName}")
+    limits = httpx.Limits(max_connections=WORKER_COUNT, max_keepalive_connections=WORKER_COUNT)
+    transport = httpx.AsyncHTTPTransport(retries=RETRY_COUNT)
 
-    cardFileBaseName = ReplaceInvalidCharacters(f"[{cardReleaseTime}][{cardCharacterName}][{cardRarity}] {cardName}")
+    async with httpx.AsyncClient(limits=limits, transport=transport, timeout=DEFAULT_TIMEOUT) as client:
+        logging.info("Fetching card data...")
 
-    DownloadCardArt(f"{CARD_ART_URL_PREFIX}{cardAssetId}{CARD_ART_STAGE1_URL_SUFFIX}", CARD_ART_DIRECTORY / cardReleaseYear / f"{cardFileBaseName}.png")
-    if cardIsTrainable:
-        DownloadCardArt(f"{CARD_ART_URL_PREFIX}{cardAssetId}{CARD_ART_STAGE2_URL_SUFFIX}", CARD_ART_DIRECTORY / cardReleaseYear / f"{cardFileBaseName} [+].png")
-
-
-with ThreadPoolExecutor(max_workers=WORKER_COUNT) as executor:
-    futureToCards = {executor.submit(ProcessSingleCard, card): card for card in cards}
-
-    for future in as_completed(futureToCards):
         try:
-            future.result()
+            response = await client.get(CARD_DATA_URL)
+            response.raise_for_status()
+            cards = response.json()
         except Exception as e:
-            logging.error(f"Exception occurred during processing card #{futureToCards[future]['id']}: {e}", exc_info=True)
+            logging.error(f"Failed to fetch card data: {e}", exc_info=True)
+            sys.exit(-1)
 
-SaveJson(manifestFile, manifest)
+        logging.info("Fetching game character data...")
+
+        try:
+            response = await client.get(GAME_CHARACTER_DATA_URL)
+            response.raise_for_status()
+            gameCharacters = response.json()
+        except Exception as e:
+            logging.error(f"Failed to fetch game character data: {e}", exc_info=True)
+            sys.exit(-1)
+
+        characterNames = {
+            gameCharacter["id"]: f"{gameCharacter.get('firstName', '')} {gameCharacter.get('givenName', '')}".strip() for gameCharacter in gameCharacters
+        }
+
+        logging.info("Downloading card art...")
+
+        async def DownloadCardArt(url: str, filePath: Path) -> bool:
+            filePath.parent.mkdir(parents=True, exist_ok=True)
+
+            if manifest.get(filePath.name):
+                return True
+
+            try:
+                async with client.stream("GET", url) as response:
+                    if response.status_code == 404:
+                        return True
+                    response.raise_for_status()
+
+                    logging.info(f"Downloading \"{filePath.name}\"")
+
+                    with filePath.open("wb") as file:
+                        async for chunk in response.aiter_bytes(chunk_size=4096):
+                            file.write(chunk)
+
+                manifest[filePath.name] = url
+
+                return True
+            except Exception as e:
+                logging.error(f"Failed to download card art \"{url}\" to \"{filePath}\": {e}", exc_info=True)
+
+                if filePath.exists():
+                    filePath.unlink()
+
+                return False
+
+        async def ProcessSingleCard(card: dict):
+            cardId = card.get("id", "")
+            cardAssetId = card.get("assetbundleName", "")
+
+            cardReleaseTime = datetime.datetime.fromtimestamp(card.get("releaseAt", 0) / 1000, tz=JST_TIMEZONE).strftime("%Y%m%d")
+            cardRarity = card.get("cardRarityType", "rarity_none").split('_')[-1].lower()
+            cardName = card.get("prefix", "")
+            cardCharacterId = card.get("characterId", "")
+
+            cardCharacterName = characterNames.get(cardCharacterId, "")
+
+            cardIsTrainable = cardRarity in [
+                "3",
+                "4",
+            ]
+            cardReleaseYear = cardReleaseTime[:4]
+
+            logging.info(f"Processing card #{cardId}({cardAssetId}):\t [{cardReleaseTime}] {cardCharacterName} - {cardName}")
+
+            cardFileBaseName = ReplaceInvalidCharacters(f"[{cardReleaseTime}][{cardCharacterName}][{cardRarity}] {cardName}")
+
+            await DownloadCardArt(f"{CARD_ART_URL_PREFIX}{cardAssetId}{CARD_ART_STAGE1_URL_SUFFIX}", CARD_ART_DIRECTORY / cardReleaseYear / f"{cardFileBaseName}.png")
+            if cardIsTrainable:
+                await DownloadCardArt(f"{CARD_ART_URL_PREFIX}{cardAssetId}{CARD_ART_STAGE2_URL_SUFFIX}", CARD_ART_DIRECTORY / cardReleaseYear / f"{cardFileBaseName} [+].png")
+
+        semaphore = asyncio.Semaphore(WORKER_COUNT)
+
+        async def _ProcessSingleCard(card: dict):
+            async with semaphore:
+                try:
+                    await ProcessSingleCard(card)
+                except Exception as e:
+                    logging.error(f"Exception occurred during processing card #{card.get('id')}: {e}", exc_info=True)
+
+        tasks = [_ProcessSingleCard(card) for card in cards]
+        await asyncio.gather(*tasks)
+
+    SaveJson(manifestFile, manifest)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
